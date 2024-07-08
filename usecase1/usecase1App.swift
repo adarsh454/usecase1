b1130d89b6112a7d7ef22c01f3020bff7cdd530a
Adarsh <adarshchaurasia06@gmail.com>
2024-07-08 07:02:15 +0530

    Initial Commit

---
+++ usecase1/usecase1App.swift
@@ -0,0 +1,17 @@
+//
+//  usecase1App.swift
+//  usecase1
+//
+//  Created by Adarsh Chaurasia on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct usecase1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
